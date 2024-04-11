dfd4da6638138dfbbbc68c50b45d28bf6a738f1b
Sarah Wu <wusarah9@gmail.com>
2024-04-10 23:28:22 -0700

    trying to fix imgs in pages

--- src/components/window/content/AboutMe.tsx
+++ src/components/window/content/AboutMe.tsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
         <div className='about-me'>
             <div className='about-me-container'>
                 <div className='about-me-header border'>
-                    <img className='me-img' src='/src/assets/imgs/pikachu.gif' alt='pixil-me.png'/>
+                    <img className='me-img' src='/kettchup/src/assets/imgs/pikachu.gif' alt='pixil-me.png'/>
                     <h1>About Me</h1>
                 </div>
                 <div className='border'>
@@ -21,7 +21,7 @@ const AboutMe = () => {
                         programming:
                     </h2>
                     <p>
-                        <img src='/src/assets/imgs/computer9.gif'/> I enjoy front-end web development, and my favourite to use currently is React because components allow for better organization and shorter files which is helpful when you're someone who gets confused by lots of information at once like me :P. Also have just started using TypeScript with it, and it's so convenient in catching mistakes during the development process.
+                        <img src='/kettchup/src/assets/imgs/computer9.gif'/> I enjoy front-end web development, and my favourite to use currently is React because components allow for better organization and shorter files which is helpful when you're someone who gets confused by lots of information at once like me :P. Also have just started using TypeScript with it, and it's so convenient in catching mistakes during the development process.
                     </p>
                     <p>
                         Recently, I've been trying to branch out and learn Node.js and Java Spring Boot to have a better understanding of all the levels of web development. It's difficult, but I'm starting to get the hang of it! <img src='src/assets/imgs/bunny46.gif' />
@@ -32,21 +32,21 @@ const AboutMe = () => {
                         games i've been playing:
                     </h2>
                     <ul>
-                        <li>honkai star rail <img src='/src/assets/imgs/star100.gif' /></li>
+                        <li>honkai star rail <img src='/kettchup/src/assets/imgs/star100.gif' /></li>
                         <ul>
                             <li className='nestedListItem'>my friend rolled acheron for me in only 23 rolls!!!</li>
                         </ul>
                         <li>stardew valley</li>
                         <li>don't starve together</li>
                         <ul>
-                            <li className='nestedListItem'>i like growing giant crops <img src='/src/assets/imgs/carrot2.gif' /></li>
+                            <li className='nestedListItem'>i like growing giant crops <img src='/kettchup/src/assets/imgs/carrot2.gif' /></li>
                         </ul>
                         <li>apex</li>
                     </ul>
                 </div>
                 <div className='border'>
                     <h2 className='font-mindset'>
-                        recent songs:<img src='/src/assets/imgs/musicnotes.gif'/>
+                        recent songs:<img src='/kettchup/src/assets/imgs/musicnotes.gif'/>
                     </h2>
                     <ul>
                         <li>illit - magnetic</li>
--- src/components/window/content/Welcome.tsx
+++ src/components/window/content/Welcome.tsx
@@ -6,13 +6,13 @@ const Welcome = () => {
             <div className='welcome-container'>
                 <div className='welcome-header'>
                     <div className='dog-div'>
-                        <img className='dog' src='/src/assets/imgs/dog-wagging-tail-white-dog.gif'/>
+                        <img className='dog' src='/kettchup/src/assets/imgs/dog-wagging-tail-white-dog.gif'/>
                     </div>
                     <h1>Welcome to kettchup's pc!</h1>
                 </div>
                 <p>Hi hi hi!! I'm kettchup or Sarah, and welcome to my personal website! I built this site after discovering Neocities and Nekoweb and was inspired by all the lovely creations to make my own.</p>
                 <p>This site is built from scratch with React and TypeScript and is not meant for mobile. I'll slowly be adding random fun things im interested here.</p>
-                <p>I hope your time spent here is fun! <img src='/src/assets/imgs/bunny25.gif' /></p>
+                <p>I hope your time spent here is fun! <img src='/kettchup/src/assets/imgs/bunny25.gif' /></p>
             </div>
         </div>
     );
