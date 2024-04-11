201c86ed0c707366a793c6d4701948e289ecc349
Sarah Wu <wusarah9@gmail.com>
2024-04-11 13:39:41 -0700

    fix scanlines url and changed opacity

--- src/components/window/content/Welcome.tsx
+++ src/components/window/content/Welcome.tsx
@@ -11,7 +11,7 @@ const Welcome = () => {
                     <h1>Welcome to kettchup's pc!</h1>
                 </div>
                 <p>Hi hi hi!! I'm kettchup or Sarah, and welcome to my personal website! I built this site after discovering Neocities and Nekoweb and was inspired by all the lovely creations to make my own.</p>
-                <p>This site is built from scratch with React and TypeScript and is not meant for mobile. I'll slowly be adding random fun things im interested here.</p>
+                <p>This site is built from scratch with React and TypeScript and is not meant for mobile. I'll slowly be adding random fun things im interested in here.</p>
                 <p>I hope your time spent here is fun! <img src='assets/imgs/bunny25.gif' /></p>
             </div>
         </div>
